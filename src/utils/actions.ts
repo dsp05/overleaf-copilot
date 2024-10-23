9dfccb7abb20d1edb9ed1ce061deb00b6bf6cf82
dsp05 <dusongpei@gmail.com>
2024-10-23 16:36:10 -0500

    Dispatch diff changes when replacing text

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,4 +1,5 @@
 import { EditorContent } from "../types";
+import * as Diff from 'diff';
 
 export function onAcceptImprovement(
   e: CustomEvent<{ improvement: string; from: number; to: number }>
@@ -11,11 +12,39 @@ export function onAcceptImprovement(
     state.selection.main.from == e.detail.from &&
     state.selection.main.to == e.detail.to
   ) {
-    const changes = {
-      from: e.detail.from,
-      to: e.detail.to,
-      insert: e.detail.improvement,
-    };
+    const originalContent = state.sliceDoc(
+      state.selection.main.from,
+      state.selection.main.to
+    )
+    const changes = [];
+    const diffs = Diff.diffWordsWithSpace(originalContent, e.detail.improvement);
+    if (diffs.length >= 500) {
+      changes.push({
+        from: e.detail.from,
+        to: e.detail.to,
+        insert: e.detail.improvement,
+      });
+    } else {
+      let index = 0;
+      for (const diff of diffs) {
+        if (diff.added) {
+          changes.push({
+            from: e.detail.from + index,
+            to: e.detail.from + index,
+            insert: diff.value,
+          });
+        } else if (diff.removed) {
+          changes.push({
+            from: e.detail.from + index,
+            to: e.detail.from + index + diff.value.length,
+          });
+          index += diff.value.length;
+        } else {
+          index += diff.value.length;
+        }
+      }
+    }
+
     const selection = { anchor: e.detail.from + e.detail.improvement.length };
     content.cmView.view.dispatch({ changes, selection });
   }
